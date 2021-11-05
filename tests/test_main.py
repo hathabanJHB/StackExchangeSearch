9f62967268e861ba9867beb180e42dce99f857c5
hathabanJHb <hathaban@student.wethinkcode.co.za>
2021-11-05 23:15:54 +0200

    test for get error

---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+import main
+
+class TestMain(TestCase):
+
+    def test_get_error_correct(self):
+        msg  = main.get_error('python3 testingFile.py')
+        self.assertEqual(msg,"TypeError: unsupported operand type(s) for +: 'int' and 'str'") 
