2c8c2f9958933b554f66b8f8d0158e8809a11cb6
hathabanJHb <hathaban@student.wethinkcode.co.za>
2021-11-05 23:27:01 +0200

    get error fuction

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-def get_error():
-    pass    
+from subprocess import Popen,PIPE
+
+
+def get_error(cmd):
+    args = cmd.split()
+    proc = Popen(args, stdout=PIPE, stderr=PIPE)
+    out,err = proc.communicate()
+    if err:
+        return err.decode('utf-8').strip().split('\n')[-1]
+
+    return "NO ERROR FOUND"
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,5 +4,5 @@ import main
 class TestMain(TestCase):
 
     def test_get_error_correct(self):
-        msg  = main.get_error('python3 testingFile.py')
+        msg  = main.get_error('python3 tests/testingFile.py')
         self.assertEqual(msg,"TypeError: unsupported operand type(s) for +: 'int' and 'str'") 
